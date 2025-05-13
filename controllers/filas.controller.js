f97f5514f3ab180793fc81cf8a1de7a8acdf6c2d
Fulano de Tal <fulanodetal@exemplo.br>
2025-05-13 11:19:51 -0300

    aula 13.05

--- controllers/filas.controller.js
+++ controllers/filas.controller.js
@@ -1,12 +1,28 @@
 const mysql = require("../mysql");
 
-exports.entrarFila = async (req, res, next) => {
+exports.verificarBrinquedo = async (req, res) => {
+    try{
+       const resultado = await mysql.execute(`
+        SELECT * FROM rides WHERE id = ?;
+        `, [req.params.idRide]);
+
+    if (resultado.length == 0) {
+        return res.status(404).send({"Mensagem": "Brinquedo não encontrado"});
+       }
+        next();
+    }catch (error){
+        return res.status(500).send(error);
+    }
+}
+
+exports.entrarFila = async (req, res) => {
     try {
-        const resultados = await mysql.execute(``,[]);(`
-        INSERT INTO lines (id_user, id_rides) VALUES (?,?)
-      `, [res.locals.idUsuario, req.params.idRide]);
-      return res.status(201).send({"Mensagens": resultados})
+        const resultados = await mysql.execute(`
+                INSERT INTO hopihari_dbx.lines (id_user, id_rides) VALUES (?,?)
+            `, [res.locals.idUsuario, Number (req.params.idRide)]);
+      return res.status(201).send({"Mensagens": resultados});
     }catch (error) {
         return res.status(500).send(error)
     }
-} 
+}
+
---
+++ dist/DML.sql
@@ -0,0 +1,40 @@
+INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'Maria', 'Silva', 'maria.silva@email.com', '1234', '1990-05-15', '1499198-6666');
+
+INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'Guilherme', 'Devito', 'Gui.Barba@email.com', '1234', '1989-07-23', '1499198-8888');
+
+INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'João', 'biston', 'Joãov.Biston@email.com', '1234', '1996-12-8', '1499198-5656');
+
+
+
+INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+VALUES (1, 'Montezum', 900, "Em manutenção", area1 );  
+
+INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+VALUES (2, 'Giranda-Mundi', 120, "Em andamento", area1);  
+
+INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+VALUES (3, 'Splash Aquático', 600,  "Fechado", area2); 
+
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (1, 1, 1, '2025-05-13 09:30:00', 1);
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (2, 2, 1, '2025-05-13 09:35:00', 2);
+
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (3, 3, 2, '2025-05-13 10:15:00', 1);
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (4, 1, 2, '2025-05-13 10:20:00', 2);
+
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (5, 2, 3, '2025-05-13 11:05:00', 1);
+
+INSERT INTO Filas (id_fila, id_usuario, id_ride, hora_entrada, posicao) 
+VALUES (6, 3, 3, '2025-05-13 11:10:00', 2);
