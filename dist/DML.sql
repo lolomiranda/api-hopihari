c10152e9567717eb238af57e41cdbda49006591e
lolomiranda <lohannypmiranda@gmail.com>
2025-05-19 17:17:56 -0300

    aula 19.05

--- dist/DML.sql
+++ dist/DML.sql
@@ -1,21 +1,21 @@
-INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
-VALUES (1, 'Maria', 'Silva', 'maria.silva@email.com', '1234', '1990-05-15', '1499198-6666');
+INSERT INTO Users (id, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'João', 'Biston', 'Joãov.Biston@email.com', '1234', '1990-05-15', '1499198-6666');
 
-INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
-VALUES (1, 'Guilherme', 'Devito', 'Gui.Barba@email.com', '1234', '1989-07-23', '1499198-8888');
+INSERT INTO Users (id, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'Guilherme', 'Paglioni', 'Gui.Bixa@email.com', '1234', '1989-07-23', '1499198-8888');
 
-INSERT INTO Usuario (id_usuario, first_name, last_name, email, password, birth_date, phone) 
-VALUES (1, 'João', 'biston', 'Joãov.Biston@email.com', '1234', '1996-12-8', '1499198-5656');
+INSERT INTO Users (id, first_name, last_name, email, password, birth_date, phone) 
+VALUES (1, 'Bruna', 'Muniz', 'MunizBru@email.com', '1234', '1996-12-8', '1499198-5656');
 
 
 
-INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+INSERT INTO rides (id_ride, name, waiting_time, status, area) 
 VALUES (1, 'Montezum', 900, "Em manutenção", area1 );  
 
-INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+INSERT INTO rides (id_ride, name, waiting_time, status, area) 
 VALUES (2, 'Giranda-Mundi', 120, "Em andamento", area1);  
 
-INSERT INTO brinquedo (id_ride, name, waiting_time, status, area) 
+INSERT INTO rides (id_ride, name, waiting_time, status, area) 
 VALUES (3, 'Splash Aquático', 600,  "Fechado", area2); 
 
 
